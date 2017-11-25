4671972ba548a01d9c527998294e8d4ec6f4b639
redsphinx <gabi.ras@hotmail.com>
2017-11-26 00:49:57 +0100

    api call done

--- api_call.py
+++ api_call.py
@@ -11,13 +11,26 @@ headers = {
 params = urllib.urlencode({
 })
 
-try:
-    conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
-    conn.request("POST", "/emotion/v1.0/recognize?faceRectangles={faceRectangles}&%s" % params, "{body}", headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+body = "{ 'url': 'https://i.pinimg.com/originals/24/63/65/246365c4e7bd4439382590ef4a149e63.jpg' }"
 
+
+def image_2_emotion(threshold):
+    try:
+        conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
+        # conn.request("POST", "/emotion/v1.0/recognize?faceRectangles={faceRectangles}&%s" % params, body, headers)
+        conn.request("POST", "/emotion/v1.0/recognize?%s" % params, body, headers)
+        response = conn.getresponse()
+        data = response.read()
+        happiness = float(data.split('"scores"')[-1].split('"happiness"')[-1].split(':')[1].split(',')[0])
+
+        if happiness >= threshold:
+            print('is happy: %f' % happiness)
+        else:
+            print('is not happy: %f' % happiness)
+
+        conn.close()
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+
+image_2_emotion(0.7)
